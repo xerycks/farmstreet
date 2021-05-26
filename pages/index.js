bf82f4fbfa596acf8359f66a3750b2c416d90dd6
xerycks <xerycks@gmail.com>
2021-05-27 01:39:11 +0530

    testing login/signup in production

--- pages/index.js
+++ pages/index.js
@@ -1,44 +1,86 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Navbar from '../component/navbar'
+import fetch from 'isomorphic-unfetch'
+import useSWR from 'swr';
+import Link from 'next/link';
+import cookie from 'js-cookie';
 
-export default function Home() {
+function Home() {
+  const {data, revalidate} = useSWR('/api/me', async function(args) {
+    const res = await fetch(args);
+    return res.json();
+  });
+  if (!data) return <h1>Loading...</h1>;
+  let loggedIn = false;
+  if (data.email) {
+    loggedIn = true;
+  }
   return (
-    <div classNameName="row">
-        <nav className="navbar navbar-expand-lg navbar-light px-0 py-3">
-    <div className="container-xl max-w-screen-xl">        
-        <a className="navbar-brand" href="#">
-            <img src="https://preview.webpixels.io/web/img/logos/clever-dark.svg" className="h-8" alt="..." />
-        </a>        
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
-            <span className="navbar-toggler-icon"></span>
-        </button>        
-        <div className="collapse navbar-collapse" id="navbarCollapse">            
-            <ul className="navbar-nav mx-lg-auto">
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Home</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Product</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Features</a>
-                </li>
-                <li className="nav-item">
-                    <a className="nav-link" href="#">Pricing</a>
-                </li>
-            </ul>            
-            <div className="navbar-nav ms-lg-4">
-                <a className="nav-item nav-link" href="#">Sign in</a>
-            </div>            
-            <div className="d-flex align-items-lg-center mt-3 mt-lg-0">
-                <a href="#" className="btn btn-sm btn-primary w-full w-lg-auto">
-                    Register
-                </a>
-            </div>
-        </div>
-    </div>
-</nav>
+    <div>
+      <Head>
+        <title>Welcome to landing page</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <h1>Simplest login</h1>
 
+      <h2>Proudly using Next.js, Mongodb and deployed with Now</h2>
+      {loggedIn && (
+        <>
+          <p>Welcome {data.email}!</p>
+          <button
+            onClick={() => {
+              cookie.remove('token');
+              revalidate();
+            }}>
+            Logout
+          </button>
+        </>
+      )}
+      {!loggedIn && (
+        <>
+          <Link href="/login">Login</Link>
+          <p>or</p>
+          <Link href="/signup">Sign Up</Link>
+        </>
+      )}
     </div>
-  )
+  );
 }
+
+export default Home;
+
+
+// export default function Home() {
+//   return (
+//     <div classNameName="row">
+// <Navbar />
+//         <div className="pt-24 pb-32">
+//     <div className="container-lg max-w-screen-xl">        
+//         <div className="row g-6 align-items-center">
+//             <div className="col-lg-7 mx-auto text-lg-center">                
+//                 <h1 className="ls-tight font-bolder display-4 mb-7 col">
+//                     <span className="text-success">FarmStreeT</span> - Stock Market of Farmers of India
+//                 </h1>                
+//                 <p className="lead mb-12">
+//                     Invest in the "Mehnat" of Farmers and see a great ROI on the crops that you invested in, with upto ~29% ROI. This
+//                 </p>                    
+//                 <form className="w-2/3 mx-auto">
+//                     <div className="rounded px-2 py-2 border shadow">
+//                         <div className="row">
+//                             <div className="col">
+//                                 <input type="email" name="email" className="form-control pl-3 shadow-none bg-transparent border-0" placeholder="Enter your email address" />
+//                             </div>
+//                             <div className="col-auto">
+//                                 <button type="submit" className="btn btn-block btn-dark">Notify me</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+//     </div>
+//   )
+// }
--- pages/login.js
+++ pages/login.js
@@ -1,37 +1,95 @@
 import Navbar from '../component/navbar'
+import React, {useState} from 'react';
+import Router from 'next/router';
+import cookie from 'js-cookie';
 
 const Login = () => {
-    return ( 
-        <>
-        <Navbar />
-        <div className="pt-24 pb-32">
-    <div className="container-lg max-w-screen-xl">        
-        <div className="row g-6 align-items-center">
-            <div className="col-lg-7 mx-auto text-lg-center">                
-                <h1 className="ls-tight font-bolder display-4 mb-7 col">
-                    <span className="text-success">FarmStreeT</span> - Stock Market of Farmers of India
-                </h1>                
-                <p className="lead mb-12">
-                    Invest in the "Mehnat" of Farmers and see a great ROI on the crops that you invested in, with upto ~29% ROI.
-                </p>                    
-                <form className="w-2/3 mx-auto">
-                    <div className="rounded px-2 py-2 border shadow">
-                        <div className="row">
-                            <div className="col">
-                                <input type="email" name="email" className="form-control pl-3 shadow-none bg-transparent border-0" placeholder="Enter your email address" />
-                            </div>
-                            <div className="col-auto">
-                                <button type="submit" className="btn btn-block btn-dark">Notify me</button>
-                            </div>
-                        </div>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-        </>
-     );
-}
- 
+  const [loginError, setLoginError] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    //call api
+    fetch('/api/auth', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        email,
+        password,
+      }),
+    })
+      .then((r) => {
+        return r.json();
+      })
+      .then((data) => {
+        if (data && data.error) {
+          setLoginError(data.message);
+        }
+        if (data && data.token) {
+          //set cookie
+          cookie.set('token', data.token, {expires: 2});
+          Router.push('/');
+        }
+      });
+  }
+  return (
+    <form onSubmit={handleSubmit}>
+      <p>Login</p>
+      <input
+        name="email"
+        type="email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
+      <input
+        name="password"
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
+      <input type="submit" value="Submit" />
+      {loginError && <p style={{color: 'red'}}>{loginError}</p>}
+    </form>
+  );
+};
+
 export default Login;
+
+// const Login = () => {
+//     return ( 
+//         <>
+//         <Navbar />
+//         <div className="pt-24 pb-32">
+//     <div className="container-lg max-w-screen-xl">        
+//         <div className="row g-6 align-items-center">
+//             <div className="col-lg-7 mx-auto text-lg-center">                
+//                 <h1 className="ls-tight font-bolder display-4 mb-7 col">
+//                     <span className="text-success">FarmStreeT</span> - Stock Market of Farmers of India
+//                 </h1>                
+//                 <p className="lead mb-12">
+//                     Invest in the "Mehnat" of Farmers and see a great ROI on the crops that you invested in, with upto ~29% ROI. This
+//                 </p>                    
+//                 <form className="w-2/3 mx-auto">
+//                     <div className="rounded px-2 py-2 border shadow">
+//                         <div className="row">
+//                             <div className="col">
+//                                 <input type="email" name="email" className="form-control pl-3 shadow-none bg-transparent border-0" placeholder="Enter your email address" />
+//                             </div>
+//                             <div className="col-auto">
+//                                 <button type="submit" className="btn btn-block btn-dark">Notify me</button>
+//                             </div>
+//                         </div>
+//                     </div>
+//                 </form>
+//             </div>
+//         </div>
+//     </div>
+// </div>
+//         </>
+//      );
+// }
+ 
+// export default Login;
